c3471b4d7a4851c1b6898c53b7f242f682618fa9
SCDN01 <116152025+SCDN01@users.noreply.github.com>
2023-03-12 04:48:44 +0800

    Add spawn and checkpoint

---
+++ Assets/Scripts/CheckPoint.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPoint : MonoBehaviour
+{
+    Spawn spawn;
+
+    private void Awake()
+    {
+        spawn = GameObject.FindGameObjectWithTag("Player").GetComponent<Spawn>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            spawn.updateCheckPoint(transform.position); 
+        }
+    }
+
+}
