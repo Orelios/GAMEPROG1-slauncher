9034678c8022f7396c0bfe0d6f79a29e5a0152c7
Cronix025 <patricktrent75@gmail.com>
2023-03-14 15:18:00 +0800

    Add enemy detection and player position

--- Assets/Scripts/EnemyBullet.cs
+++ Assets/Scripts/EnemyBullet.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class EnemyBullet : MonoBehaviour
 {
+    private GameObject player; //Used to search fo the player
+    private Rigidbody2D rb; //Used for physics
     public float speed = 3.0f; //Determines how fast the bullet travels
     public float lifeTime = 3.0f; //Determines how long the bullet last
     public float damage = 1.0f; //Determines how much damage the bullet has
@@ -11,13 +13,12 @@ public class EnemyBullet : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
         Destroy(this.gameObject, lifeTime); //Destroy the object
-    }
+        player = GameObject.FindGameObjectWithTag("Player"); //Defines the tag of the player
 
-    // Update is called once per frame
-    void Update()
-    {
-        transform.position += Vector3.left * speed * Time.deltaTime;
+        Vector3 direction = player.transform.position - transform.position; //Searches the player's position
+        rb.velocity = new Vector2(direction.x, direction.y).normalized * speed; //Bullet goes straight to player
     }
 
     void OnTriggerEnter2D(Collider2D collider)
--- Assets/Scripts/EnemyShoot.cs
+++ Assets/Scripts/EnemyShoot.cs
@@ -5,24 +5,37 @@ using UnityEngine;
 public class EnemyShoot : MonoBehaviour
 {
     public GameObject enemyBullet; //What the enemy will spawn/shoot out
+    public GameObject player; //Searches for the player
     private float recharge = 0.0f; //Variable based on time
     public float fireRate = 3.0f; //Cooldown of shooting
+    public float atkRange = 5.0f; //Defines the detection of the Enemy
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
 
     // Update is called once per frame
     void Update()
     {
-        //The moment the enemy spawn, they will shoot
-        if (recharge > 0)
+
+        float distance = Vector2.Distance(transform.position, player.transform.position);
+
+        if (distance < atkRange)
         {
-            recharge -= Time.deltaTime; //Reduces recharge's cooldown
-        }
+            //The moment the enemy spawn, they will shoot
+            if (recharge > 0)
+            {
+                recharge -= Time.deltaTime; //Reduces recharge's cooldown
+            }
 
 
-        if (recharge <= 0)
-        {
+            if (recharge <= 0)
+            {
 
-            Instantiate(enemyBullet, transform.position, Quaternion.identity); //Spawn bullet
-            recharge = fireRate; //Reset the variable
-        }
+                Instantiate(enemyBullet, transform.position, Quaternion.identity); //Spawn bullet
+                recharge = fireRate; //Reset the variable
+            }
+        } 
     }
 }
