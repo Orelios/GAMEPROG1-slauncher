e365ac93622b0a1dedf18d14ad29e75c628ffb90
Cronix025 <patricktrent75@gmail.com>
2023-03-16 15:39:39 +0800

    Add enemy movement and enemy ground checkers

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -4,18 +4,39 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
-    private float horizontal; //Used for the horizontal inputs
-    public float moveSpeed = 8f; //Used to determine the speed of the player
+    [SerializeField] float moveSpeed = 8f; //Used to determine the speed of the player
+    public GameObject groundChecker; //Uses a seperate object within the enemy to make the collider
+    Rigidbody2D rb;
+    BoxCollider2D bc;
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        bc = groundChecker.GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (IsFacingRight())
+        {
+            rb.velocity = new Vector2(moveSpeed, 0f); //Move right
+        }
+        else
+        {
+            rb.velocity = new Vector2(-moveSpeed, 0f); //Move left
+        }
+    }
+
+    private bool IsFacingRight() //Checks if the character is facing right
+    {
+        return transform.localScale.x > Mathf.Epsilon;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision) //If the collider doesn't collide with anything, it will flip
+    {
+        transform.localScale = new Vector2(-(Mathf.Sign(rb.velocity.x)), transform.localScale.y);
     }
 }
