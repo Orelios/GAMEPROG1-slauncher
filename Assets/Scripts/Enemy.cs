c8bd9393d56618151ee56fa405e289d40fca1e66
Cronix025 <patricktrent75@gmail.com>
2023-03-28 20:53:17 +0800

    Add playerbullet interaction with enemy

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -14,12 +14,12 @@ public class Enemy : MonoBehaviour
         health = maxHealth;
     }
 
-    private void OnTriggerStay2D(Collider2D other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        /*if(other.tag == "Player")
+        if (other.tag == "PlayerBullet")
         {
             health -= 1.0f;
-        }*/
+        }
 
         if (health <= 0)
         {
