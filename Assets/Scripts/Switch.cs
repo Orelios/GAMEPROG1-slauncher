ebfaddfe249f8877f8a341d34da4bff43f7ff585
Floof-i <justincuy12@gmail.com>
2023-04-01 01:30:26 +0800

    add prefabs, switches, moving platforms

--- Assets/Scripts/Switch.cs
+++ Assets/Scripts/Switch.cs
@@ -5,25 +5,45 @@ using UnityEngine;
 public class Switch : MonoBehaviour
 {
     public bool active = false;
+    public bool pressurePlate;
+    public Player player;
+    private Animator anim; //Used to reference parameters from the animator
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        UpdateAnimationState();
     }
 
     private void OnTriggerStay2D(Collider2D other)
     {
-        if(other.gameObject.tag == "Player")
+        if(other.gameObject.tag == "Player" && pressurePlate == false)
         {
             active = true;
         }
+
+        if(other.gameObject.tag == "Player" && pressurePlate == true)
+        {
+            if(player.health  > 8)
+            {
+                active =true;
+            }
+        }
+    }
+
+    private void UpdateAnimationState()
+    {
+        if(active == true)
+        {
+            anim.SetBool("active", true);
+        }
+
     }
 
 }
--- Assets/Scripts/SwitchMechanism.cs
+++ Assets/Scripts/SwitchMechanism.cs
@@ -6,8 +6,11 @@ public class SwitchMechanism : MonoBehaviour
 {
     public Switch playerSwitch;
     public float yMove = -3;
+    public float xMove = 6;
     public float moveVelocity = -1;
+    public bool horizontal;
     private float desiredYPosition;
+    private float desiredXPosition;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,7 +20,7 @@ public class SwitchMechanism : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(yMove < 0 && playerSwitch.active == true)
+        if(yMove < 0 && playerSwitch.active == true && horizontal == false)
         {
             if(transform.position.y > desiredYPosition)
             {
