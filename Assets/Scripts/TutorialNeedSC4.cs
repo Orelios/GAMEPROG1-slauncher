195b5dc781333133e16d2ce139df2d5ef831f28d
vongabrieltan <vongabrieltan@gmail.com>
2023-04-02 18:40:20 +0800

    Add script TutorialNeedSC4

---
+++ Assets/Scripts/TutorialNeedSC4.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TutorialNeedSC4 : MonoBehaviour
+{
+    TextMeshProUGUI text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GetComponent<TextMeshProUGUI>();
+        text.text = " ";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ShowText()
+    {
+        text.text = "2 SC4s needed to destroy barrier";
+    }
+}
